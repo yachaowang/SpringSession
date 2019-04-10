6e187c3d313539ae7d32fa06048ad793e95ad812
zs <zs@bjpowernode.com>
2019-04-10 11:32:28 +0800

    SpringSession实现Session共享

---
+++ src/main/java/com/bjpowernode/session/servlet/SetSession.java
@@ -0,0 +1,20 @@
+package com.bjpowernode.session.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/setSession")
+public class SetSession extends HttpServlet {
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().setAttribute("web","http://www.bjpowernode.com");
+        response.getWriter().write("ok");
+    }
+}
---
+++ src/main/java/com/bjpowernode/session/servlet/getSession.java
@@ -0,0 +1,20 @@
+package com.bjpowernode.session.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/getSession")
+public class getSession extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       String web = (String) request.getSession().getAttribute("web");
+       response.getWriter().write(web==null?"NO Session":web);
+    }
+}
